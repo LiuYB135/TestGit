c485c28d8ebb4caa4b848355e7a91c4925b918c8
eclipse_user <eclipse_user@lyb.com>
2018-09-04 19:41:18 +0800

    This is local develop's edit!

--- src/main/java/com/lyb/git/Happy.java
+++ src/main/java/com/lyb/git/Happy.java
@@ -3,7 +3,7 @@ package com.lyb.git;
 public class Happy {
 
 	public static void main(String[] args) {
-		System.out.println("left .....");
+		System.out.println("left and right ......");
 		System.out.println("This is develop's edit!!");
 	}
 }
